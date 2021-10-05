03b7b5ae91ebbdc24b00ad091cd1efb45469be5d
Willian S <wsapcf@gmail.com>
2021-10-04 22:08:01 -0300

    add Authentication usecase

---
+++ lib/domain/usecases/authentication.dart
@@ -0,0 +1,7 @@
+import '../entities/account_entity.dart';
+import 'package:meta/meta.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth(
+      {@required String email, @required String password});
+}
