c7fbb4429b0d93fa88b0033b500ae082efb1488b
Willian S <wsapcf@gmail.com>
2021-10-05 22:40:14 -0300

    AuthenticationParams refactor

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -2,6 +2,12 @@ import '../entities/account_entity.dart';
 import 'package:meta/meta.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth(
-      {@required String email, @required String password});
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({@required this.email, @required this.secret});
 }
