333846469d6fed786ac4bbe901e778e012445676
Willian S <wsapcf@gmail.com>
2021-10-05 23:22:28 -0300

    write unexpected test

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -4,6 +4,7 @@ import 'package:mockito/mockito.dart';
 import 'package:clean_trivia/domain/usecases/usecases.dart';
 import 'package:clean_trivia/data/usecases/usecases.dart';
 import 'package:clean_trivia/data/http/http.dart';
+import 'package:clean_trivia/domain/helpers/helpers.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -29,14 +30,21 @@ void main() {
       'password': params.secret,
     }));
   });
-  test('Should call HttpClient with correct url and empty body', () async {
+  test('should throw UnexpectedError if HttpClient returns 400', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
     final params = AuthenticationParams(
       email: faker.internet.email(),
       secret: faker.internet.password(),
     );
-    when(httpClient.request(url: url, method: 'post'))
-        .thenAnswer((_) async => null);
-    await sut.auth(params);
-    verify(httpClient.request(url: url, method: 'post', body: {}));
+    final future = sut.auth(params);
+    verify(httpClient.request(url: url, method: 'post', body: {
+      'email': params.email,
+      'password': params.secret,
+    }));
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
