9ed5462dffde83a9ae0b2e50738903f50213a1f5
Willian S <wsapcf@gmail.com>
2021-10-05 23:26:38 -0300

    ensure RemoteAuthentication throw HttpError.notFound on 404

--- requirements/remote_authentication_use_case.md
+++ requirements/remote_authentication_use_case.md
@@ -9,7 +9,7 @@
 
 ## Exceção - URL inválida
 
-1. Sistema retorna a mensagem de erro inesperado
+1. ✅ Sistema retorna a mensagem de erro inesperado
 
 ## Exceção - Dados inválidos
 
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -45,4 +45,18 @@ void main() {
     }));
     expect(future, throwsA(DomainError.unexpected));
   });
+  test('should throw UnexpectedError if HttpClient returns 404', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.notFound);
+
+    final future = sut.auth(params);
+    verify(httpClient.request(url: url, method: 'post', body: {
+      'email': params.email,
+      'password': params.secret,
+    }));
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
